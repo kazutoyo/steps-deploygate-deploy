e66936da4f39019b0ee4f9730d013bffbeb4a8a1
kazutoyo <tokai@kazutoyo.tk>
2016-09-05 17:44:44 +0900

    check variable

--- step.sh
+++ step.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 APP_BINARY_PATH="$app_binary_path"
@@ -9,6 +7,15 @@ DEPLOYGATE_USER_NAME="$deploy_gate_user_name"
 DEPLOYGATE_API_KEY="$deploy_gate_api_key"
 DEPLOYGATE_DISTRIBUTION_KEY="$deploy_gate_distribution_key"
 DEPLOY_GATE_INSTALL_URL="$deploy_gate_install_url"
+
+echo "==== VARIABLE ======"
+echo $APP_BINARY_PATH
+echo $DEPLOYGATE_USER_NAME
+echo "$deploy_gate_user_name"
+echo $DEPLOYGATE_API_KEY
+echo $DEPLOYGATE_DISTRIBUTION_KEY
+echo $DEPLOYGATE_INSTALL_URL
+echo "==================="
 
 echo "***************************"
 echo "* Uploading to DeployGate *"
