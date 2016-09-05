9c9e4d5c610752ae6cda96a85ced6d1a1005738b
kazutoyo <tokai@kazutoyo.tk>
2016-09-05 20:08:40 +0900

    fix

--- step.sh
+++ step.sh
@@ -2,18 +2,11 @@
 
 THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-APP_BINARY_PATH="$app_binary_path"
-DEPLOYGATE_USER_NAME="$deploy_gate_user_name"
-DEPLOYGATE_API_KEY="$deploy_gate_api_key"
-DEPLOYGATE_DISTRIBUTION_KEY="$deploy_gate_distribution_key"
-
 echo "==== VARIABLE ======"
-echo $app_binary_path
-echo $deploy_gate_user_name
-echo $deploy_gate_api_key
-echo $deploy_gate_distribution_key
-echo $deploy_gate_install_url
+echo $APP_BINARY_PATH
+echo $DEPLOYGATE_USER_NAME
 echo $DEPLOYGATE_API_KEY
+echo $DEPLOYGATE_DISTRIBUTION_KEY
 echo $DEPLOYGATE_INSTALL_URL
 echo "==================="
 
